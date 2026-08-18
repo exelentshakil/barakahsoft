33e739dd380eddc35549e8d9926bc8134419db94
exelentshakil <exelentshakil@gmail.com>
2026-08-18 06:23:41 +0600

    Add support chat and client portal prototypes

---
+++ src/app/client-portal-prototype/page.tsx
@@ -0,0 +1,8 @@
+import { CrispChat } from "@/components/CrispChat";
+import { ClientPortalPrototype } from "@/components/client/ClientPortalPrototype";
+
+export const metadata = { title: "Client Portal Prototype | BarakahSoft" };
+
+export default function ClientPortalPrototypePage() {
+  return <><ClientPortalPrototype /><CrispChat /></>;
+}
